a94a2a28cbe707ccae455cd3972d16e40ee21c34
sqrmyy <dn41.ra20adtdjw@gmail.com>
2020-11-04 07:51:41 +0900

    fix App.js

--- App.js
+++ App.js
@@ -18,24 +18,10 @@ const RootStack = createStackNavigator(
   }
 );
 
+const AppContainer = createAppContainer(RootStack);
+
 export default class App extends React.Component {
   render() {
-    const Layout = createAppContainer(RootStack);
-    return (
-      // <View style={styles.container}>
-      //   <Text>Open up App.js to start working on your app!</Text>
-      //   <StatusBar style='auto' />
-      // </View>
-      <Layout />
-    );
+    return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
